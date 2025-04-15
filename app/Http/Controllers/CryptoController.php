e0783c3cbe103f120c89289a1ddc74f3d158fab3
Mubashir Zamir <zamir.mubashir@gmail.com>
2025-04-15 14:49:25 +0100

    Changed responses to be explicitly JSON and improved decode function to return 404 when non-existent URL is sent.

--- app/Http/Controllers/CryptoController.php
+++ app/Http/Controllers/CryptoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Contracts\CryptoService;
 use App\Http\Requests\CryptoRequest;
 use App\Services\DefaultCryptoService;
+use Illuminate\Http\JsonResponse;
 
 class CryptoController extends Controller
 {
@@ -19,13 +20,19 @@ class CryptoController extends Controller
         $this->cryptoService = $cryptoService;
     }
 
-    public function encode(CryptoRequest $request): array
+    public function encode(CryptoRequest $request): JsonResponse
     {
-        return ['url' => $this->cryptoService->encode($request->input('url'))];
+        return response()->json(['url' => $this->cryptoService->encode($request->input('url'))]);
     }
 
-    public function decode(CryptoRequest $request): array
+    public function decode(CryptoRequest $request): JsonResponse
     {
-        return ['url' => $this->cryptoService->decode($request->input('url'))];
+        $decode = $this->cryptoService->decode($request->input('url'));
+
+        if ($decode === null) {
+            return response()->json(['error' => 'URL not found'], 404);
+        }
+
+        return response()->json(['url' => $decode]);
     }
 }
